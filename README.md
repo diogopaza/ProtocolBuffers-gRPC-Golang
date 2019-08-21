c5e68649a914d380a4a993f55ce3e787fe8760eb
diogopaza <diogopazacvel@gmail.com>
2019-08-21 17:36:07 -0300

    2

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 <h1></h1>
+<p>https://www.youtube.com/watch?v=_jQ3i_fyqGA</p>
 <p>Os arquivos .proto definem estruturas serializadas pelo protocol buffer.</p>
 <p>O comando <strong>protoc --go_out=. nomeArquivo</strong> gera código manualmente para o Go.</p>
-<p><strong>go generate</strong> gera o arquivo atraves de um comentario colocado no inicio do arquivo go</p>
+<p><strong>go generate</strong> gera o arquivo atraves de uma tag colocada no inicio do arquivo go</p>
 <p>Exemplo para ser usado diretamente em um arquivo go <em>//go:generate protoc --go_out=. ./user/user.proto
 </em></p>
--- main.go
+++ main.go
@@ -1,7 +1,54 @@
-//go:generate protoc --go_out=. ./user.proto
-
 package main
+
+import (
+	"flag"
+	"fmt"
+	//"github.com/proto/protobuf/proto"
+	"github.com/golang/protobuf/proto"
+	"os"
+	"protobuf/todo"
+	"strings"
+	
+)
 
 func main() {
 
+	flag.Parse()
+	if flag.NArg() < 1 {
+		fmt.Println(os.Stderr, "missing subcommand: list or add")
+		os.Exit(1)
+	}
+	var err error
+	
+	switch cmd := flag.Arg(0); cmd {
+	case "list":
+		err = list()
+	case "add":
+		err = add(strings.Join(flag.Args()[1:]," "))
+	default:
+		err = fmt.Errorf("unknown subcommand %s ", cmd)
+	}
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	
+}
+
+
+//const dbPath = "mydb.pb"
+
+func add(text string) error {
+	task := &todo.Task{
+		Text: text,
+		Done: false,
+	}
+	fmt.Println(proto.MarshalTextString(task))
+	//fmt.Println(task)
+	return nil
+}
+
+func list() error {
+	return nil
 }
